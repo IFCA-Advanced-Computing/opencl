350d633735a80f1586ffeedd6476146660e957c9
Luis Cabellos <zhen.sydow@gmail.com>
2011-07-11 17:28:50 +0200

    fix header

--- src/System/GPU/OpenCL.hs
+++ src/System/GPU/OpenCL.hs
@@ -1,5 +1,5 @@
 -- -----------------------------------------------------------------------------
--- This file is part of Skema.
+-- This file is part of Haskell-Opencl.
 
 -- Haskell-Opencl is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
