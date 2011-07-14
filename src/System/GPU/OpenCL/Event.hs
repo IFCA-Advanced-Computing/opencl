3f6af9b10f403842f3359abab18b269e9873008d
Luis Cabellos <zhen.sydow@gmail.com>
2011-07-14 13:43:15 +0200

    add Event functions

--- src/System/GPU/OpenCL/Event.hs
+++ src/System/GPU/OpenCL/Event.hs
@@ -17,10 +17,145 @@
 {-# LANGUAGE ForeignFunctionInterface, ScopedTypeVariables #-}
 module System.GPU.OpenCL.Event(  
   -- * Types
-  CLEvent
+  CLint, CLEvent, CLCommandType(..), CLProfilingInfo(..), 
+  CLCommandExecutionStatus(..),
+  -- * Functions
+  clWaitForEvents, clRetainEvent, clReleaseEvent, clGetEventCommandQueue, 
+  clGetEventCommandType, clGetEventReferenceCount, 
+  clGetEventCommandExecutionStatus, clGetEventProfilingInfo
   ) where
 
 -- -----------------------------------------------------------------------------
-import System.GPU.OpenCL.Types( CLEvent )
+import Foreign( Ptr, castPtr, nullPtr, alloca, allocaArray, peek, pokeArray )
+import Foreign.C.Types( CSize )
+import Foreign.Storable( sizeOf )
+import System.GPU.OpenCL.Types( 
+  CLEvent, CLint, CLuint, CLulong, CLEventInfo_, CLProfilingInfo_, 
+  CLCommandQueue, CLCommandType(..), CLCommandType_, CLCommandExecutionStatus(..), 
+  CLProfilingInfo(..), getCommandType, getCommandExecutionStatus, getProfilingInfoValue )
+import System.GPU.OpenCL.Errors( clSuccess, ErrorCode(..) )
+
+-- -----------------------------------------------------------------------------
+foreign import ccall "clWaitForEvents" raw_clWaitForEvents :: 
+  CLuint -> Ptr CLEvent -> IO CLint
+foreign import ccall "clGetEventInfo" raw_clGetEventInfo :: 
+  CLEvent -> CLEventInfo_ -> CSize -> Ptr () -> Ptr CSize -> IO CLint
+foreign import ccall "clRetainEvent" raw_clRetainEvent :: 
+  CLEvent -> IO CLint 
+foreign import ccall "clReleaseEvent" raw_clReleaseEvent :: 
+  CLEvent -> IO CLint 
+foreign import ccall "clGetEventProfilingInfo" raw_clGetEventProfilingInfo :: 
+  CLEvent -> CLProfilingInfo_ -> CSize -> Ptr () -> Ptr CSize -> IO CLint
+
+-- -----------------------------------------------------------------------------
+-- | Waits on the host thread for commands identified by event objects in 
+-- event_list to complete. A command is considered complete if its execution 
+-- status is 'CL_COMPLETE' or a negative value.
+-- Returns 'True' if the function was executed successfully. It returns 'False'
+-- if the list of events is empty, or if events specified in event_list do not 
+-- belong to the same context, or if event objects specified in event_list are 
+-- not valid event objects.
+clWaitForEvents :: [CLEvent] -> IO Bool
+clWaitForEvents [] = return False
+clWaitForEvents xs = allocaArray nevents $ \pevents -> do
+  pokeArray pevents xs
+  errcode <- fmap ErrorCode $ raw_clWaitForEvents (fromIntegral nevents) pevents
+  return (errcode==clSuccess) 
+    where
+      nevents = length xs
+  
+-- | Increments the event reference count.
+-- The OpenCL commands that return an event perform an implicit retain.
+-- Returns 'True' if the function is executed successfully. It returns 'False' 
+-- if event is not a valid event object.
+clRetainEvent :: CLEvent -> IO Bool
+clRetainEvent ev = raw_clRetainEvent ev
+                   >>= return . (==clSuccess) . ErrorCode
+
+-- | Decrements the event reference count.
+-- Decrements the event reference count. The event object is deleted once the 
+-- reference count becomes zero, the specific command identified by this event 
+-- has completed (or terminated) and there are no commands in the command-queues 
+-- of a context that require a wait for this event to complete.
+-- Returns 'True' if the function is executed successfully. It returns 'False' 
+-- if event is not a valid event object.
+clReleaseEvent :: CLEvent -> IO Bool
+clReleaseEvent ev = raw_clReleaseEvent ev
+                    >>= return . (==clSuccess) . ErrorCode
+
+-- | Return the command-queue associated with event.
+clGetEventCommandQueue :: CLEvent -> IO (Maybe CLCommandQueue)
+clGetEventCommandQueue ev = alloca $ \(dat :: Ptr CLCommandQueue) -> do
+  errcode <- fmap ErrorCode $ raw_clGetEventInfo ev 0x11D0 size (castPtr dat) nullPtr
+  if errcode == clSuccess
+    then fmap Just $ peek dat
+    else return Nothing
+    where 
+      size = fromIntegral $ sizeOf (nullPtr::CLCommandQueue)
+
+-- | Return the command associated with event.
+clGetEventCommandType :: CLEvent -> IO (Maybe CLCommandType)
+clGetEventCommandType ev = alloca $ \(dat :: Ptr CLCommandType_) -> do
+  errcode <- fmap ErrorCode $ raw_clGetEventInfo ev 0x11D1 size (castPtr dat) nullPtr
+  if errcode == clSuccess
+    then fmap getCommandType $ peek dat
+    else return Nothing
+    where 
+      size = fromIntegral $ sizeOf (0::CLCommandType_)
+
+-- | Return the event reference count. The reference count returned should be 
+-- considered immediately stale. It is unsuitable for general use in applications. 
+-- This feature is provided for identifying memory leaks.
+clGetEventReferenceCount :: CLEvent -> IO (Maybe CLint)
+clGetEventReferenceCount ev = alloca $ \(dat :: Ptr CLint) -> do
+  errcode <- fmap ErrorCode $ raw_clGetEventInfo ev 0x11D2 size (castPtr dat) nullPtr
+  if errcode == clSuccess
+    then fmap Just $ peek dat
+    else return Nothing
+    where 
+      size = fromIntegral $ sizeOf (0::CLint)
+
+-- | Return the execution status of the command identified by event.
+clGetEventCommandExecutionStatus :: CLEvent -> IO (Maybe CLCommandExecutionStatus)
+clGetEventCommandExecutionStatus ev = alloca $ \(dat :: Ptr CLint) -> do
+  errcode <- fmap ErrorCode $ raw_clGetEventInfo ev 0x11D3 size (castPtr dat) nullPtr
+  if errcode == clSuccess
+    then fmap (getCommandExecutionStatus) $ peek dat
+    else return Nothing
+    where 
+      size = fromIntegral $ sizeOf (0::CLint)
+      
+{-| Returns profiling information for the command associated with event if 
+profiling is enabled. The unsigned 64-bit values returned can be used to measure 
+the time in nano-seconds consumed by OpenCL commands.
+
+OpenCL devices are required to correctly track time across changes in device 
+frequency and power states. The 'CL_DEVICE_PROFILING_TIMER_RESOLUTION' specifies 
+the resolution of the timer i.e. the number of nanoseconds elapsed before the 
+timer is incremented.
+
+Event objects can be used to capture profiling information that measure 
+execution time of a command. Profiling of OpenCL commands can be enabled either 
+by using a command-queue created with 'CL_QUEUE_PROFILING_ENABLE' flag set in 
+properties argument to clCreateCommandQueue or by setting the 
+'CL_QUEUE_PROFILING_ENABLE' flag in properties argument to 
+'clSetCommandQueueProperty'.
+
+'clGetEventProfilingInfo' returns the valueif the function is executed 
+successfully and the profiling information has been recorded, and returns 
+'Nothing'  if the 'CL_QUEUE_PROFILING_ENABLE' flag is not set for the 
+command-queue and if the profiling information is currently not available 
+(because the command identified by event has not completed), or if event is a 
+not a valid event object.
+-} 
+clGetEventProfilingInfo :: CLEvent -> CLProfilingInfo -> IO (Maybe CLulong)
+clGetEventProfilingInfo ev prof = alloca $ \(dat :: Ptr CLulong) -> do
+  errcode <- fmap ErrorCode $ raw_clGetEventProfilingInfo ev infoid size (castPtr dat) nullPtr
+  if errcode == clSuccess
+    then fmap Just $ peek dat
+    else return Nothing
+    where 
+      size = fromIntegral $ sizeOf (0::CLulong)
+      infoid = getProfilingInfoValue prof
 
 -- -----------------------------------------------------------------------------
--- src/System/GPU/OpenCL/Types.chs
+++ src/System/GPU/OpenCL/Types.chs
@@ -21,10 +21,12 @@ module System.GPU.OpenCL.Types(
   CLDeviceMemCacheType(..), CLDeviceExecCapability(..), CLDeviceLocalMemType(..),
   CLPlatformID, CLDeviceID, CLContext, CLCommandQueue, CLContextProperty_,
   CLDeviceType(..), CLCommandQueueProperty(..), CLCommandQueueInfo_, 
+  CLEventInfo_, CLProfilingInfo_, CLCommandType, CLCommandType_, 
+  CLCommandExecutionStatus(..), CLProfilingInfo(..), getProfilingInfoValue,
   CLCommandQueueProperty_, getDeviceTypeValue, getDeviceLocalMemType, 
-  getDeviceMemCacheType, bitmaskToDeviceTypes, bitmaskFromDeviceTypes, 
-  bitmaskToCommandQueueProperties, bitmaskFromCommandQueueProperties, 
-  bitmaskToFPConfig, bitmaskToExecCapability ) 
+  getDeviceMemCacheType, getCommandType, getCommandExecutionStatus, 
+  bitmaskToDeviceTypes, bitmaskFromDeviceTypes, bitmaskToCommandQueueProperties, 
+  bitmaskFromCommandQueueProperties, bitmaskToFPConfig, bitmaskToExecCapability )
        where
 
 -- -----------------------------------------------------------------------------
@@ -62,7 +64,10 @@ type CLDeviceExecCapability_ = {#type cl_device_exec_capabilities#}
 type CLContextInfo_ = {#type cl_context_info#}
 type CLContextProperty_ = {#type cl_context_properties#}
 type CLCommandQueueInfo_ = {#type cl_command_queue_info#}
-type CLCommandQueueProperty_ ={#type cl_command_queue_properties#}
+type CLCommandQueueProperty_ = {#type cl_command_queue_properties#}
+type CLEventInfo_ = {#type cl_event_info#}
+type CLProfilingInfo_ = {#type cl_profiling_info#}
+type CLCommandType_ = {#type cl_command_type#}
 
 newtype ErrorCode = ErrorCode CInt deriving( Eq )
 
@@ -155,6 +160,83 @@ deviceLocalMemTypes :: [(CLDeviceLocalMemType_,CLDeviceLocalMemType)]
 deviceLocalMemTypes = [(0x1,CL_LOCAL), (0x2,CL_GLOBAL)]
 getDeviceLocalMemType :: CLDeviceLocalMemType_ -> Maybe CLDeviceLocalMemType
 getDeviceLocalMemType val = lookup val deviceLocalMemTypes
+
+data CLCommandType = CL_COMMAND_NDRANGE_KERNEL | CL_COMMAND_TASK 
+                   | CL_COMMAND_NATIVE_KERNEL | CL_COMMAND_READ_BUFFER
+                   | CL_COMMAND_WRITE_BUFFER | CL_COMMAND_COPY_BUFFER
+                   | CL_COMMAND_READ_IMAGE | CL_COMMAND_WRITE_IMAGE
+                   | CL_COMMAND_COPY_IMAGE | CL_COMMAND_COPY_BUFFER_TO_IMAGE
+                   | CL_COMMAND_COPY_IMAGE_TO_BUFFER | CL_COMMAND_MAP_BUFFER
+                   | CL_COMMAND_MAP_IMAGE | CL_COMMAND_UNMAP_MEM_OBJECT
+                   | CL_COMMAND_MARKER | CL_COMMAND_ACQUIRE_GL_OBJECTS
+                   | CL_COMMAND_RELEASE_GL_OBJECTS
+                   deriving( Show )
+
+commandTypes :: [(CLCommandType_, CLCommandType)]
+commandTypes = [(0x11F0,CL_COMMAND_NDRANGE_KERNEL),(0x11F1,CL_COMMAND_TASK),
+                (0x11F2,CL_COMMAND_NATIVE_KERNEL),(0x11F3,CL_COMMAND_READ_BUFFER),
+                (0x11F4,CL_COMMAND_WRITE_BUFFER),(0x11F5,CL_COMMAND_COPY_BUFFER),
+                (0x11F6,CL_COMMAND_READ_IMAGE),(0x11F7,CL_COMMAND_WRITE_IMAGE),
+                (0x11F8,CL_COMMAND_COPY_IMAGE),(0x11F9,CL_COMMAND_COPY_IMAGE_TO_BUFFER),
+                (0x11FA,CL_COMMAND_COPY_BUFFER_TO_IMAGE),(0x11FB,CL_COMMAND_MAP_BUFFER),
+                (0x11FC,CL_COMMAND_MAP_IMAGE),(0x11FD,CL_COMMAND_UNMAP_MEM_OBJECT),
+                (0x11FE,CL_COMMAND_MARKER),(0x11FF,CL_COMMAND_ACQUIRE_GL_OBJECTS),
+                (0x1200,CL_COMMAND_RELEASE_GL_OBJECTS)]
+getCommandType :: CLCommandType_ -> Maybe CLCommandType
+getCommandType = (`lookup` commandTypes)
+
+data CLCommandExecutionStatus = CL_QUEUED 
+                                -- ^ command has been enqueued in the command-queue
+                              | CL_SUBMITTED 
+                                -- ^ enqueued command has been submitted by the 
+                                -- host to the device associated with the 
+                                -- command-queue
+                              | CL_RUNNING 
+                                -- ^ device is currently executing this command
+                              | CL_COMPLETE -- ^ the command has completed
+                              | CL_EXEC_ERROR 
+                                -- ^ command was abnormally terminated
+                                
+commandExecutionStatus :: [(CLint, CLCommandExecutionStatus)]
+commandExecutionStatus = [(0x0,CL_COMPLETE),(0x1,CL_RUNNING),
+                          (0x2,CL_SUBMITTED),(0x3,CL_QUEUED)]
+getCommandExecutionStatus :: CLint -> Maybe CLCommandExecutionStatus                                
+getCommandExecutionStatus n 
+  | n < 0 = Just CL_EXEC_ERROR
+  | otherwise = lookup n commandExecutionStatus
+                
+#c
+enum CLProfilingInfo {
+  CLPROFILING_COMMAND_QUEUED = CL_PROFILING_COMMAND_QUEUED,
+  CLPROFILING_COMMAND_SUBMIT = CL_PROFILING_COMMAND_SUBMIT,
+  CLPROFILING_COMMAND_START = CL_PROFILING_COMMAND_START,
+  CLPROFILING_COMMAND_END = CL_PROFILING_COMMAND_END
+  };
+#endc
+
+{-| Specifies the profiling data.
+
+ * 'CLPROFILING_COMMAND_QUEUED', A 64-bit value that describes the current 
+device time counter in nanoseconds when the command identified by event is 
+enqueued in a command-queue by the host.
+ 
+ * 'CLPROFILING_COMMAND_SUBMIT', A 64-bit value that describes the current 
+device time counter in nanoseconds when the command identified by event that has 
+been enqueued is submitted by the host to the device associated with the 
+commandqueue.
+ 
+ * 'CLPROFILING_COMMAND_START',	 A 64-bit value that describes the current 
+device time counter in nanoseconds when the command identified by event starts 
+execution on the device.
+ 
+ * 'CLPROFILING_COMMAND_END', A 64-bit value that describes the current device 
+time counter in nanoseconds when the command identified by event has finished 
+execution on the device.
+-}
+{#enum CLProfilingInfo {}#}
+
+getProfilingInfoValue :: CLProfilingInfo -> CLProfilingInfo_
+getProfilingInfoValue = fromIntegral . fromEnum
 
 -- -----------------------------------------------------------------------------
 bitmaskToDeviceTypes :: CLDeviceType_ -> [CLDeviceType]
